609bacf14b3085bc7e7440b54ce93020e7888fcf
rosnidwitaa <rosnidwitaa@gmail.com>
2024-05-15 23:48:33 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="parent">
+    <h1>Parent Component</h1>
+    <child-component @emitData="handleData">
+      <template v-slot:childSlot="{ data }">
+        <div class="child-slot">
+          {{ data }}
+        </div>
+      </template>
+    </child-component>
+    <div class="received-data" v-if="receivedData">
+      <p>Data yang diterima: {{ receivedData }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import ChildComponent from './components/ChildComponent.vue';
+
+export default {
+  components: {
+    ChildComponent
+  },
+  data() {
+    return {
+      receivedData: null
+    }
+  },
+  methods: {
+    handleData(data) {
+      this.receivedData = data;
+    }
+  }
+}
+</script>
+
+<style scoped>
+.parent {
+  font-family: Arial, sans-serif;
+  padding: 20px;
+}
+
+.received-data {
+  margin-top: 20px;
+  background-color: #408db9;
+  padding: 10px;
+  border-radius: 5px;
+}
+
+.child-slot {
+  background-color: #32566b;
+  padding: 10px;
+  border-radius: 5px;
+}
+</style>
---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="child">
+    <h2>Child Component</h2>
+    <button @click="emitData">Kirim Data</button>
+    <slot name="childSlot" :data="data"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      data: "Rosni Dwita - 223510686"
+    };
+  },
+  methods: {
+    emitData() {
+      this.$emit('emitData', this.data);
+    }
+  }
+}
+</script>
+
+<style scoped>
+.child {
+  font-family: Arial, sans-serif;
+  border: 1px solid #ccc;
+  padding: 20px;
+  border-radius: 5px;
+}
+
+button {
+  padding: 10px 20px;
+  background-color: #273646;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+button:hover {
+  background-color: #2f4052;
+}
+</style>
